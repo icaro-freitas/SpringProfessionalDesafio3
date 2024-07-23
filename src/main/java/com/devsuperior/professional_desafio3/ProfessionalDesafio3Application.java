d2d57c12951f876ec9af1a873e2f276b5f2bf703
icaro-freitas <icaro_freitas2014@outlook.com>
2024-07-22 22:42:43 -0300

    First commit

---
+++ src/main/java/com/devsuperior/professional_desafio3/ProfessionalDesafio3Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.professional_desafio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalDesafio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalDesafio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/professional_desafio3/ProfessionalDesafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.professional_desafio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalDesafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
